fb0115b04bbc5f7bc82569cfe659e952ae1d37e4
KINGMAINA2343 <136616917+KINGLEE2343@users.noreply.github.com>
2025-05-18 13:43:56 +0300

    Update music adim.py

--- music adim.py
+++ music adim.py
@@ -5,3 +5,13 @@ from .models import Artist, Song, Playlist
 admin.site.register(Artist)
 admin.site.register(Song)
 admin.site.register(Playlist)
+# my_music_app/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('music.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
